39da6028b6fcc63514afacee245424cc48e1e12f
Mvptong <chanawee.jmp@gmail.com>
2023-08-20 11:10:53 +0700

    Add user login

--- client.py
+++ client.py
@@ -1,11 +1,83 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
+
+class LoginWindow(tk.Toplevel):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.title("Login")
+        
+        self.label_username = tk.Label(self, text="Username")
+        self.label_password = tk.Label(self, text="Password")
+        self.entry_username = tk.Entry(self)
+        self.entry_password = tk.Entry(self, show="*")
+
+        self.label_username.pack(pady=10)
+        self.entry_username.pack(pady=10)
+        self.label_password.pack(pady=10)
+        self.entry_password.pack(pady=10)
+
+        self.button_login = tk.Button(self, text="Login", command=self.validate_login)
+        self.button_login.pack(pady=10)
+
+    def validate_login(self):
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+
+        if (username, password) in self.master.user_data:
+            role = self.master.user_data[(username, password)]
+            self.master.login_success(role, username)
+            self.destroy()
+        else:
+            messagebox.showerror("Invalid Credentials", "Invalid username or password")
+
+class AddUserWindow(tk.Toplevel):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.title("Add User")
+        
+        self.label_username = tk.Label(self, text="Username")
+        self.label_password = tk.Label(self, text="Password")
+        self.label_role = tk.Label(self, text="Role (Admin/Normal)")
+        
+        self.entry_username = tk.Entry(self)
+        self.entry_password = tk.Entry(self, show="*")
+        self.entry_role = tk.Entry(self)
+
+        self.label_username.pack(pady=10)
+        self.entry_username.pack(pady=10)
+        self.label_password.pack(pady=10)
+        self.entry_password.pack(pady=10)
+        self.label_role.pack(pady=10)
+        self.entry_role.pack(pady=10)
+
+        self.button_add = tk.Button(self, text="Add User", command=self.add_user)
+        self.button_add.pack(pady=10)
+
+    def add_user(self):
+        new_username = self.entry_username.get()
+        new_password = self.entry_password.get()
+        new_role = self.entry_role.get()
+
+        if new_role not in ['Admin', 'Normal']:
+            messagebox.showerror("Invalid Role", "Role must be Admin or Normal")
+            return
+        
+        if (new_username, new_password) in self.master.user_data:
+            messagebox.showerror("User Exists", "User with this username and password already exists")
+            return
+
+        self.master.user_data[(new_username, new_password)] = new_role
+        messagebox.showinfo("Success", "User added successfully!")
+        self.destroy()
 
 class GoldStockApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('ห้างทองหวังทองดี')
         
+        # ... (your original __init__ code for GoldStockApp)
         # Set window size
         self.window_width = 1000  # Updated width
         self.window_height = 500
@@ -28,8 +100,28 @@ class GoldStockApp(tk.Tk):
         self.init_add_gold_interface()
         
         self.show_main_interface()
-    
-    def init_main_interface(self):
+
+        self.user_data = {
+            ('admin', 'VTDprogram2566'): 'Admin',
+            # Add more users as necessary
+        }
+
+        self.login_window = LoginWindow(self)
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        
+    def login_success(self, role, username):
+        if role == 'Admin':
+            self.show_main_interface()
+        else:
+            self.show_main_interface(username)
+
+    def on_closing(self):
+        if self.login_window:
+            self.login_window.destroy()
+        self.destroy()
+        
+    def init_main_interface(self, username=None):
+        # ... (your original init_main_interface code for GoldStockApp)
         self.tree_frame = ttk.Frame(self.main_frame)
         self.tree_frame.pack(pady=20)
         
@@ -40,7 +132,22 @@ class GoldStockApp(tk.Tk):
         
         self.edit_button = tk.Button(self.main_frame, text="Edit Gold Stock", command=self.edit_gold_stock)
         self.edit_button.pack(pady=20)
-        
+
+        if username:
+            self.filter_stocks_by_user(username)
+        else:
+            self.add_user_button = tk.Button(self.main_frame, text="Add User", command=self.show_add_user_interface)
+            self.add_user_button.pack(pady=20)
+            
+    def show_add_user_interface(self):
+        AddUserWindow(self)
+
+    def filter_stocks_by_user(self, username):
+        for item in self.tree.get_children():
+            values = self.tree.item(item, 'values')
+            if values[6] != username:
+                self.tree.delete(item)
+    
     def init_tree(self, frame):
         self.tree_scroll = ttk.Scrollbar(frame)
         self.tree_scroll.pack(side=tk.RIGHT, fill=tk.Y)
@@ -53,7 +160,11 @@ class GoldStockApp(tk.Tk):
         for col in ('วันที่', 'SKU', 'หยิบทองที่', 'ประเภท', 'น้ำหนัก', 'จำนวน', 'User', 'สถานะ'):
             self.tree.column(col, width=100)
             self.tree.heading(col, text=col)
-        
+    
+    def show_add_gold_interface(self):
+        self.main_frame.pack_forget()
+        self.add_frame.pack(fill="both", expand=True)
+    
     def init_add_gold_interface(self):
         self.entries = {}
         
@@ -74,10 +185,6 @@ class GoldStockApp(tk.Tk):
         self.add_frame.pack_forget()
         self.main_frame.pack(fill="both", expand=True)
         
-    def show_add_gold_interface(self):
-        self.main_frame.pack_forget()
-        self.add_frame.pack(fill="both", expand=True)
-        
     def add_gold_to_table(self):
         values = [self.entries[col].get() for col in ('วันที่', 'SKU', 'หยิบทองที่', 'ประเภท', 'น้ำหนัก', 'จำนวน', 'User', 'สถานะ')]
         self.tree.insert('', 'end', values=values)
@@ -85,7 +192,8 @@ class GoldStockApp(tk.Tk):
 
     def edit_gold_stock(self):
         EditStockWindow(self)
-        
+
+# ... (your original EditStockWindow and EditStockDetailsWindow classes)
 class EditStockWindow(tk.Toplevel):
     def __init__(self, master):
         super().__init__(master)
@@ -153,5 +261,6 @@ class EditStockDetailsWindow(tk.Toplevel):
         self.destroy()
 
 # Create and start the application
-app = GoldStockApp()
-app.mainloop()
+if __name__ == '__main__':
+    app = GoldStockApp()
+    app.mainloop()
